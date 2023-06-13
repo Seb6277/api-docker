575d45984b67c40ff60f450a1c50233ef4c71952
Seb6277 <sebastien.carpentier89@gmail.com>
2023-06-13 10:35:52 +0200

    update 2023

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const mongodbUrl = "mongodb+srv://application:test@cluster0-sqljl.mongodb.net/test?retryWrites=true&w=majority";
+const mongodbUrl = "mongodb+srv://seb:test@cluster0.loi4tec.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 app.use(express.json());
